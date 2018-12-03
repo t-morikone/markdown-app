42b3ae1a44311983bf0ab49484e84ce1d1a65e4d
“takehzi” <luminox726@gmail.com>
2018-12-03 10:46:25 +0900

    implementation_login.js2

--- routes/login.js
+++ routes/login.js
@@ -18,3 +18,61 @@ let Bookshelf = require('bookshelf')(knex);
 let User = Bookshelf.Model.extend({
   tableName: 'users'
 });
+
+router.get('/' ,(req, res, next) => {
+  let data = {
+    title: 'Login',
+    form: {name: '', password:''},
+    content: '名前とパスワードを入力してください'
+  }
+  res.render('login', data);
+});
+
+router.post('/', (req, res, next) => {
+  let request = req;
+  let response = res;
+  req.check('name', 'NAME は必ず入力してください').notEmpty();
+  req.check('password', 'PASSWORD は必ず入力してください').notEmpty();
+  req.getValidationResult().then((result) => {
+    if (!result.isEmpty()){
+      let content = '<ul class="error" >';
+      let result_arr = result.array();
+      for (let n in result_arr) {
+        content += '<li>' + result_arr[i].msg + '</li>'
+      }
+      content += '</ul>';
+      let data = {
+        title: 'Login',
+        content: content,
+        form: req.body
+      }
+      response.render('login', data);
+    } else {
+      let nm = req.body.name;
+      let pw = req.body.password;
+
+      User.query({where: {name: nm}, andWhere: {password: pw}})
+        .fetch()
+        .then((model)=>{
+          if(model = null){
+            let data = {
+              title: '再入力',
+              content: '<p class="error">名前またはパスワードが違います。</p>',
+              form: req.body
+            };
+            response.render('login', data);
+          } else {
+            request.session.login = model.attributes;
+            let data = {
+              title: 'Login',
+              content: '<p>ログインしました！<br>トップページに戻ってメッセージを送信ください。</p>',
+              form: req.body
+            };
+            response.render('login', data);
+          }
+        });
+    }
+  })
+});
+
+module.exports = router;
